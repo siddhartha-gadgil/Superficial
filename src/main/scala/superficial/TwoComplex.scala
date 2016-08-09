e55549632d3b245cdce2cf13c12419cc945696a0
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2016-08-09 08:36:14 +0530

    Basics of two-complexes; no quotients yet.

--- src/main/scala/superficial/TwoComplex.scala
+++ src/main/scala/superficial/TwoComplex.scala
@@ -1,10 +1,12 @@
 package superficial
 
 class Polygon(val sides: Int) extends TwoComplex{
-  lazy val polygons = Set(this)
+  lazy val faces = Set(this)
 
   lazy val edges : Set[Edge] =
     (for (e <- 0 to (sides - 1); ori <- Set(true, false)) yield PolygonEdge(this, e, ori)).toSet
+    
+  lazy val vertices : Set[Vertex] = ((0 to (sides -1)) map (PolygonVertex(this, _))).toSet
 }
 
 trait Vertex
@@ -17,15 +19,16 @@ trait Edge{
   def tail: Vertex
 }
 
-case class PolygonEdge(polygon: Polygon, edge: Int, positiveOriented: Boolean) extends Edge{
-  lazy val flip : Edge = PolygonEdge(polygon, edge, !(positiveOriented))
+case class PolygonEdge(polygon: Polygon, index: Int, positiveOriented: Boolean) extends Edge{
+  lazy val flip : Edge = PolygonEdge(polygon, index, !(positiveOriented))
 
-  lazy val tail = PolygonVertex(polygon, edge)
+  lazy val (head, tail)  = 
+    if (positiveOriented) (PolygonVertex(polygon, (index + 1) % polygon.sides), PolygonVertex(polygon, index))
+    else (PolygonVertex(polygon, index), PolygonVertex(polygon, (index + 1) % polygon.sides))
 
-  lazy val head = PolygonVertex(polygon, (edge + 1) % polygon.sides)
 }
 
-case class PolygonVertex(polygon: Polygon, vertex: Int) extends Vertex
+case class PolygonVertex(polygon: Polygon, index: Int) extends Vertex
 
 case class QuotientEdge(edges: Set[Edge]) extends Edge{
   def flip = QuotientEdge(edges map (_.flip))
@@ -39,6 +42,9 @@ case class QuotientEdge(edges: Set[Edge]) extends Edge{
 case class QuotientVertex(vertices : Set[Vertex]) extends Vertex
 
 trait TwoComplex{
-  def polygons: Set[Polygon]
-  def edges: Set[Edge]
+  def faces: Set[Polygon]
+
+  def edges: Set[Edge] // these come in pairs, related by flip (reversing orientation
+
+  def vertices: Set[Vertex]
 }
