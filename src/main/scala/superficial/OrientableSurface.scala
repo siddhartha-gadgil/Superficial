6c9738a38abc0d69a68820c4438c97acd3a59684
Ankit-Jaiswal <akj24ankit@gmail.com>
2016-11-15 22:13:00 +0530

    includes second kind of word reduction

--- src/main/scala/superficial/OrientableSurface.scala
+++ src/main/scala/superficial/OrientableSurface.scala
@@ -15,12 +15,13 @@ case class Inverse(gen: Generator) extends Generator
 // As of now, considering surfaces with no boundary.
 case class OrientableSurface(g: Int) {
 
-	val d: Vector[Generator] = this.g match {
+	val relation: Vector[Generator] = this.g match {
 		case 1 => Vector(a(1),b(1),Inverse(a(1)),Inverse(b(1)))
-		case g => OrientableSurface(g-1).d ++ Vector(a(g),b(g),Inverse(a(g)),Inverse(b(g)))
+		case g => OrientableSurface(g-1).relation ++ Vector(a(g),b(g),Inverse(a(g)),Inverse(b(g)))
 	}
+	val d = relation.length
 
-	def reduce(w: Word): Word = ??? // red3(red2(red1(w.xs))) but how to implement while loop
+	def reduce(w: Word): Word = ??? // red3(red2(red1(w.xs)))
 
 	def red1(xs: Vector[Generator]): Vector[Generator] = xs match {
 		case Vector() => Vector()
@@ -31,34 +32,33 @@ case class OrientableSurface(g: Int) {
 		case x +: rest => x +: red1(rest)
 	}
 
-	def red2(xs: Vector[Generator]): Vector[Generator] =
-		if (xs.containsSlice(d)) {xs.patch(xs.indexOfSlice(d),Vector(a(3)),d.length)}
-		else {Vector()}
+def red2(xs: Vector[Generator]) = subred2(xs,0,d)
 
-/*what stuff i need.
-patch
-foldbyright(Inverse) over xs
-slice
-containsSlice
-indexOfSlice
-
-
-psuedo code:
-def 2(xs) = xs++xs
-
-def red2(w) = subred2(w,0,d.length)
-def subred2(w,i,n) = {
-	val j = w.indexOfSlice(2(d).slice(i,i+n))
-
-	if ( w.containsSlice(2(d).slice(i,i+n)) )
-		{ w.slice(0,j) ++ inverse(   ) ++ subred2(w.slice(j,:),i,n) }
-	else if ( 2(w).containsSlice(2(d).slice(i,i+n)) )
-		{                               }
-	else if (i < d.length) {subred2(w,i+1,n)}
-	else if (n > d.length/2) { subred2(w,0,n-1) }
-	else { w }
+def inv(xs: Vector[Generator]) : Vector[Generator] = xs match {
+	case Vector() => Vector()
+	case Inverse(l) +: rest => inv(rest) :+ l
+	case l +: rest => inv(rest) :+ Inverse(l)
 }
 
-*/
+def relInv(xs: Vector[Generator]): Vector[Generator] = {
+	val j = (relation++relation).indexOfSlice(xs)
+	inv((relation++relation).slice(j+xs.length,j+d))
+}
+
+def subred2(xs:Vector[Generator], i:Int, n:Int): Vector[Generator] = {
+	if (n > d/2) {
+		if (i < d) {
+			if ( (xs++xs).containsSlice((relation++relation).slice(i,i+n)) ) {
+				val j = (xs++xs).indexOfSlice((relation++relation).slice(i,i+n))
+				val l = xs.length
+				if (j+n > l-1) { subred2( (xs++xs).slice(j+n+1-l,j) ++ relInv((xs++xs).slice(j,j+n)), i,n) }
+				else { subred2( xs.slice(0,j) ++ relInv(xs.slice(j,j+n)) ++ xs.slice(j+n,l-1) , i,n) }
+			}
+			else { subred2(xs,i+1,n) }
+		}
+		else { subred2(xs,0,n-1) }
+	}
+	else { xs }
+}
 
 }
