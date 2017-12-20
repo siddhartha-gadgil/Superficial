2a6cc0febc7b4142e12b9eac7504025d6f7c0466
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2017-12-20 09:17:50 +0530

    Memoized norm computations, including scaled

--- build.sbt
+++ build.sbt
@@ -4,20 +4,12 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.12.4"
 )
 
-libraryDependencies += "com.lihaoyi" % "ammonite" % "1.0.3" % "test" cross CrossVersion.full
+
 
 lazy val root = (project in file(".")).
   settings(commonSettings: _*).
   settings(
     name := "superficial",
     libraryDependencies += "io.monix" %% "monix" % "3.0.0-M2",
-    sourceGenerators in Test += Def.task {
-        val file = (sourceManaged in Test).value / "amm.scala"
-        val initCommands =
-          """import superficial._, freegroups._"""
-        IO.write(
-          file,
-          s"""object amm extends App { ammonite.Main("$initCommands").run() }""")
-        Seq(file)
-    }.taskValue
+    libraryDependencies += "com.lihaoyi" % "ammonite" % "1.0.3" cross CrossVersion.full
   )
--- src/main/scala/freegroups/LinearNorm.scala
+++ src/main/scala/freegroups/LinearNorm.scala
@@ -1,8 +1,21 @@
 package freegroups
 
-import monix.eval._
+import monix.eval._, monix.tail._, cats.implicits._
+
+import scala.collection.mutable.{Map => mMap}
+
+import monix.execution.Scheduler.Implicits.global
+
+
+object amm{
+  val initCommands =
+    """import superficial._, freegroups._, LinearNorm._"""
+  def apply() = ammonite.Main(s"$initCommands").run()
+}
 
 object LinearNorm{
+  val memoNorm: mMap[Vector[Int], Double] = mMap()
+
   def norm(word: Vector[Int]): Int = word match {
     case Vector() => 0
     case x +: Vector() => 1
@@ -13,26 +26,57 @@ object LinearNorm{
         ((1 + norm(ys)) +: recNorms).min
   }
 
-  def normTask(word: Vector[Int]): Task[Int] =
+  def normTask(word: Vector[Int]): Task[Double] =
+    memoNorm.get(word).map(Task(_)).
+    getOrElse{
     Task(word). flatMap {
       case Vector() => Task.pure(0)
       case x +: Vector() => Task.pure(1)
       case x +: ys =>
-          val matchedIndices = ys.zipWithIndex.filter(_._1 == -x).map(_._2)
-          val afterSplits = matchedIndices.map((i) => ys.splitAt(i)).map{case (a, b) => (a, b.tail)}
-          val recNormsTask = Task.gather(
-            afterSplits.map{
-              case (ta, tb) =>
-                for {
-                  a <-normTask(ta)
-                  b <- normTask(tb)
-                } yield a + b
-              }
-              )
-          for {
-            recNorms <- recNormsTask
-            ynorm <- normTask(ys)
-          } yield ((1 + ynorm) +: recNorms).min
+        if (x == - ys.last) normTask(ys.init)
+        else
+          {
+            val matchedIndices = ys.zipWithIndex.filter(_._1 == -x).map(_._2)
+            val afterSplits = matchedIndices.map((i) => ys.splitAt(i)).map{case (a, b) => (a, b.tail)}
+            val recNormsTask = Task.gather(
+              afterSplits.map{
+                case (ta, tb) =>
+                  for {
+                    a <-normTask(ta)
+                    b <- normTask(tb)
+                  } yield a + b
+                }
+                )
+            for {
+              recNorms <- recNormsTask
+              ynorm <- normTask(ys)
+              res = ((1 + ynorm) +: recNorms).min
+              _ = memoNorm += word -> res
+            } yield res
+        }
+      }
+  }
+
+  def scaledNorm(word: Vector[Int], n: Int) =
+    normTask(Vector.fill(n)(word).reduce(_ ++ _)).map{(x) =>
+      val res = x  / n
+      memoNorm.get(word).foreach{
+        (p) => if (p > res) memoNorm += (word -> res)
+      }
+      res}
+
+  def scaledTask(word: Vector[Int], start: Int, stop: Int) = {
+    val it = Iterant.range[Task](start, stop).scanEval[Vector[Double]](Task.pure(Vector())){
+        case (v, n) =>
+        for {
+          res <- scaledNorm(word, n)
+          _ = println(res)
+         } yield v :+ res
+       }
+
+    val task = it.foldL
+    task
+
   }
 
 }
