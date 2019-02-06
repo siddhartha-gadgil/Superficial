17de1445e5abcd3d186fb972b8e94416165c5f4f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-02-06 20:45:50 +0530

    some corrections, logging

--- freegroups/src/main/scala/freegroups/ProofFinder.scala
+++ freegroups/src/main/scala/freegroups/ProofFinder.scala
@@ -9,21 +9,30 @@ import monix.execution.Scheduler.Implicits.global
 import LinearNorm._, LinearNormProofs.{c, cna}
 import LinNormBound._
 
-case class PowerMove(word: Word, exp: Int, normBeforeOpt: Option[Double], normAfter: Double)
+case class PowerMove(
+    word: Word,
+    exp: Int,
+    normBeforeOpt: Option[Double],
+    normAfter: Double
+)
 
-object PowerMove{
+object PowerMove {
   def historyMap(m: Map[Word, Double], moves: Vector[PowerMove]) = {
-    val moveMap = moves.groupBy(_.word).mapValues(v => v.map(_.normBeforeOpt).flatten.min)
+    val moveMap =
+      moves.groupBy(_.word).mapValues(v => v.map(_.normBeforeOpt).flatten.min)
     m ++ moveMap
   }
 }
 
-case class NormData(norms: Map[Word, Double], moves: Vector[PowerMove]){
+case class NormData(norms: Map[Word, Double], moves: Vector[PowerMove]) {
   def takeTill(word: Word, n: Int) = {
     val head = moves.takeWhile(m => (m.word, m.exp) != (word, n))
     val tail = moves.drop(head.size)
     NormData(PowerMove.historyMap(norms, tail), head)
   }
+
+  def wordNormOpt(word: Word, norm: Double): Option[(Word, NormData)] =
+    if (norms(word) == norm) Some(word -> this) else None
 }
 
 object ProofFinder {
@@ -50,7 +59,7 @@ object ProofFinder {
 
   def egTask(n: Int) = homogeneityTask(egSeq(n))
 
-  def memMap : Map[Word, Double] =
+  def memMap: Map[Word, Double] =
     memoNorm.map { case (v, x) => Word(v) -> x }.toMap ++ Map(
       Word("a") -> 1.0,
       Word("a!") -> 1.0,
@@ -68,36 +77,45 @@ object ProofFinder {
       case x +: Vector() =>
         Some(Gen(x))
       case x +: ys =>
-        if (normData.norms(w) == 1 + normData.norms(Word(ys))) quickProof(Word(ys),normData).map { pf =>
-          Triang(Gen(x), pf)
-        } else {
+        if (normData.norms(w) == 1 + normData.norms(Word(ys)))
+          quickProof(Word(ys), normData).map { pf =>
+            Triang(Gen(x), pf)
+          } else {
+          if (normData.norms(w) > 1 + normData.norms(Word(ys)))
+            println(s"Wrong triangle inequality with ${Word(ys)} and $w")
           val matchedIndices = ys.zipWithIndex.filter(_._1 == -x).map(_._2)
           val afterSplits = matchedIndices.map((i) => ys.splitAt(i)).map {
             case (a, b) => (a, b.tail)
           }
           val matchedNorms = afterSplits.filter {
             case (a, b) =>
-              // println(Word(a))
-              // println(Word(b))
-              normData.norms(Word(a)) + normData.norms(Word(b)) == normData.norms(w)
+              if (normData.norms(Word(a)) + normData.norms(Word(b)) < normData
+                    .norms(w))
+                println(
+                  s"Wrong triangle inequality with ${Word(a)}, ${Word(b)} and $w"
+                )
+              normData.norms(Word(a)) + normData.norms(Word(b)) == normData
+                .norms(w)
           }
           matchedNorms.headOption.flatMap {
             case (a, b) =>
               for {
-                pfA <- quickProof(Word(a),normData)
-                pfB <- quickProof(Word(b),normData)
+                pfA <- quickProof(Word(a), normData)
+                pfB <- quickProof(Word(b), normData)
               } yield Triang(ConjGen(x, pfA), pfB)
           }
-        }
-      .orElse{
-          val exps = normData.moves.filter(_.word == w).map(_.exp).filter(_ > 1)
-          val nOpt = exps.find(n => normData.norms(w.fastPow(n))/n == normData.norms(w))
-          for {
+        }.orElse {
+            val exps =
+              normData.moves.filter(_.word == w).map(_.exp).filter(_ > 1)
+            val nOpt = exps.find(
+              n => normData.norms(w.fastPow(n)) / n == normData.norms(w)
+            )
+            for {
               n <- nOpt
               _ = println(n)
               _ = println(w)
-              pf <- quickProof(w, normData.takeTill(w, n))
-          } yield PowerBound(w, n, pf)
-      }
+              pf <- quickProof(w.fastPow(n), normData.takeTill(w, n))
+            } yield PowerBound(w, n, pf)
+          }
     }
 }
