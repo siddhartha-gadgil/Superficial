d494083cfb43ff19a5fde0e97c338e30e71d9e09
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-16 17:40:37 +0530

    improved bounding, good decay at 4%

--- freegroups/src/main/scala/freegroups/UnderArc.scala
+++ freegroups/src/main/scala/freegroups/UnderArc.scala
@@ -3,7 +3,7 @@ package freegroups
 import scala.collection.mutable.{Map => MMap}
 
 object UnderArc {
-  val memopad: MMap[(Int, Int), Double] = MMap()
+  val memopad: MMap[(Int, Int, Int), Double] = MMap()
 
   def withUnmatched(n: Int, k: Int): Double =
     if (n == k) math.pow(2, n + 1) - 1
@@ -21,9 +21,9 @@ object UnderArc {
       terms.sum
     }
 
-  def memoCount(n: Int, k: Int): Double =
+  def count(n: Int, k: Int, degree: Int): Double =
     memopad.getOrElse(
-      n -> k, {
+      (n, k, degree), {
         val result: Double =
           if (n == k) math.pow(2, n + 1) - 1
           else {
@@ -32,17 +32,22 @@ object UnderArc {
                 a <- 0 to n - 2
                 b <- 0 to n - 2 - a
                 k1 <- 0 to k - a
-              } yield
-                (math.pow(3, a + 1) * memoCount(b, k1) * memoCount(
+                term1 = count(b, k1, 2) * count(
                   n - 2 - a - b,
-                  k - a - k1
-                ))
+                  k - a - k1, degree
+                )
+                term2 = (count(b, k1, 3) -count(b, k1, 2)) * count(
+                  n - 2 - a - b,
+                  k - a - k1, 1
+                )
+              } yield
+                (math.pow(degree, a) * 3 * (term1 + term2)) 
             terms.sum
           }
-        memopad += (n, k) ->result 
+        memopad += (n, k, degree) ->result 
         result
       }
     )
 
-      def p(n: Int, k: Int) : Double = memoCount(n, k) / math.pow(4, n)
+      def p(n: Int, k: Int, degree: Int = 3) : Double = count(n, k, degree) / math.pow(4, n)
 }
