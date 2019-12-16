ce0630625df798adb3653171867b46f7fd274b5a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-12-16 09:03:08 +0530

    two way sync of binaries

--- bindeploy.sh
+++ bindeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-cp CATG2020/notebooks/bin/superficial-*.jar CATG2020/static/bin
+rsync -av math.iisc.ac.in:~/public_html/catg2020/bin/ CATG2020/static/bin/
+cp CATG2020/notebooks/bin/superficial-*.jar CATG2020/static/bin || true
 cd CATG2020
 hugo
 rsync -avz public/ math.iisc.ac.in:/home/gadgil/public_html/catg2020/
