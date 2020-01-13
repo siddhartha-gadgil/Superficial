10422387f67e72bbe947c452c767df9326abbd0d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-01-13 19:27:00 +0530

    symbolic surface descriptions, closes #14

--- superficial/src/main/scala/superficial/TwoComplex.scala
+++ superficial/src/main/scala/superficial/TwoComplex.scala
@@ -1,6 +1,7 @@
 package superficial
 
 import Polygon.Index
+
 /**
   * Abstract polygon, with given edges and vertices, i.e. a two-complex with a single face.
   * @param sides number of sides
@@ -10,35 +11,42 @@ trait Polygon extends TwoComplex {
 
   lazy val faces = Set(this)
 
-  lazy val indices : Vector[Index] = (0 until sides).toVector
+  lazy val indices: Vector[Index] = (0 until sides).toVector
 
   val boundary: Vector[Edge]
 
   /**
-   * the boundary as a formal sum
-   */
+    * the boundary as a formal sum
+    */
   def del: FormalSum[Edge] = {
-    val ob = boundary.collect{case oe: OrientedEdge => oe}
-    assert(ob.size == boundary.size, "computing boundary where some edge is not oriented")
-    val coeffVec = ob.map{case e => if (e.positivelyOriented) (e, 1) else (e.flip, -1)}
+    val ob = boundary.collect { case oe: OrientedEdge => oe }
+    assert(
+      ob.size == boundary.size,
+      "computing boundary where some edge is not oriented"
+    )
+    val coeffVec = ob.map {
+      case e => if (e.positivelyOriented) (e, 1) else (e.flip, -1)
+    }
     FormalSum.reduced(coeffVec)
   }
 
   /**
-   * the set of edges
-   */
+    * the set of edges
+    */
   lazy val edges: Set[Edge] =
     boundary.toSet.flatMap((s: Edge) => Set(s, s.flip))
 
   /**
-   * the set of vertices 
-   */
+    * the set of vertices
+    */
   val vertices: Set[Vertex]
 
   def boundaryIndex(e: Edge): Set[(Int, Boolean)] =
-    indices.toSet.filter{
-      (n) => e == boundary(n) || e.flip == boundary(n)
-    }.map(n => (n, e == boundary(n)))
+    indices.toSet
+      .filter { (n) =>
+        e == boundary(n) || e.flip == boundary(n)
+      }
+      .map(n => (n, e == boundary(n)))
 }
 
 object Polygon {
@@ -60,16 +68,17 @@ object Polygon {
       (indices map (PolygonVertex(self, _))).toSet
   }
 
-  case class Symbolic(name: String, boundary: Vector[Edge]) extends Polygon{
+  case class Symbolic(name: String, boundary: Vector[Edge]) extends Polygon {
     val sides: Int = boundary.size
 
     val vertices: Set[Vertex] = edges.flatMap(e => Set(e.initial, e.terminal))
   }
 
-  case class PolygonEdge(polygon: Polygon,
-                         index: Index,
-                         positiveOriented: Boolean)
-      extends Edge {
+  case class PolygonEdge(
+      polygon: Polygon,
+      index: Index,
+      positiveOriented: Boolean
+  ) extends Edge {
     lazy val flip: PolygonEdge = PolygonEdge(polygon, index, !positiveOriented)
 
     lazy val terminal: PolygonVertex =
@@ -95,7 +104,7 @@ object Polygon {
   */
 class Vertex
 
-object Vertex{
+object Vertex {
   case class Symbolic(name: String) extends Vertex
 }
 
@@ -103,34 +112,51 @@ object Vertex{
   * An oriented edge in a two-complex
   */
 trait Edge {
+
   /**
-   * the same edge with the opposite orientation.
-   */
+    * the same edge with the opposite orientation.
+    */
   def flip: Edge
 
   def terminal: Vertex
 
   def initial: Vertex
 
-  def del : FormalSum[Vertex] = FormalSum.reduced(Vector(terminal -> 1, initial -> -1))
+  def del: FormalSum[Vertex] =
+    FormalSum.reduced(Vector(terminal -> 1, initial -> -1))
 }
 
-object Edge{
-  case class Symbolic(name: String, initial: Vertex, terminal: Vertex, positivelyOriented : Boolean = true) extends OrientedEdge{
+object Edge {
+  case class Symbolic(
+      name: String,
+      initial: Vertex,
+      terminal: Vertex,
+      positivelyOriented: Boolean = true
+  ) extends OrientedEdge {
     def flip: Edge = Symbolic(name, terminal, initial, !positivelyOriented)
   }
 
-  def symbolic(name: String, initialName: String, terminalName: String, positivelyOriented : Boolean = true) = 
-    Symbolic(name, Vertex.Symbolic(initialName), Vertex.Symbolic(terminalName), positivelyOriented)
+  def symbolic(
+      name: String,
+      initialName: String,
+      terminalName: String,
+      positivelyOriented: Boolean = true
+  ) =
+    Symbolic(
+      name,
+      Vertex.Symbolic(initialName),
+      Vertex.Symbolic(terminalName),
+      positivelyOriented
+    )
 }
 
-trait OrientedEdge extends Edge{
-  val positivelyOriented : Boolean
+trait OrientedEdge extends Edge {
+  val positivelyOriented: Boolean
 }
 
 /**
- * An edge obtained by identifications
- */ 
+  * An edge obtained by identifications
+  */
 case class QuotientEdge(edges: Set[Edge]) extends Edge {
   def flip = QuotientEdge(edges map (_.flip))
 
@@ -141,24 +167,55 @@ case class QuotientEdge(edges: Set[Edge]) extends Edge {
 }
 
 /**
- * A vertex obtained by identifications
- */ 
+  * A vertex obtained by identifications
+  */
 case class QuotientVertex(vertices: Set[Vertex]) extends Vertex
 
+object TwoComplex {
+  def symbolic(
+      vertexNames: Set[String],
+      edgeMap: Map[String, (String, String)],
+      faceMap: Map[String, Vector[String]]
+  ): TwoComplex =
+    new TwoComplex {
+      lazy val vertices: Set[Vertex] = vertexNames.map(Vertex.Symbolic(_))
+      lazy val edges: Set[Edge] =
+        edgeMap.map {
+          case (e, (a, b)) => Edge.symbolic(e, a, b)
+        }.toSet
+
+      def getEdge(s: String) = edgeMap.find(_._1 == s).map {
+        case (e, (a, b)) => Edge.symbolic(e, a, b)
+      }
+
+      lazy val faces: Set[Polygon] =
+        faceMap.map {
+          case (face, vec) =>
+            val edges = for {
+              name <- vec
+            } yield getEdge(name).get
+            Polygon.Symbolic(face, edges)
+        }.toSet
+    }
+}
+
 /**
-  *  A polyheadral two complex, with faces polygons, a collection of edges and 
+  *  A polyheadral two complex, with faces polygons, a collection of edges and
   */
 trait TwoComplex {
   def faces: Set[Polygon]
 
-  def edges
-    : Set[Edge] // these come in pairs, related by flip (reversing orientation)
+  def edges: Set[Edge] // these come in pairs, related by flip (reversing orientation)
 
   lazy val positiveEdges =
-    edges.toVector.collect{case oe : OrientedEdge if oe.positivelyOriented => oe}
+    edges.toVector.collect {
+      case oe: OrientedEdge if oe.positivelyOriented => oe
+    }
 
   def edgeIndex(edge: Edge) = {
-    positiveEdges.zipWithIndex.find{case (e, i) => e == edge || e.flip == edge}.map{case (e, i) => (i, e == edge)}
+    positiveEdges.zipWithIndex
+      .find { case (e, i) => e == edge || e.flip == edge }
+      .map { case (e, i) => (i, e == edge) }
   }
 
   def vertices: Set[Vertex]
@@ -167,8 +224,8 @@ trait TwoComplex {
     faces.filter((face) => face.edges.contains(edge))
 
   def edgeIndices(edge: Edge): Set[(Polygon, Index, Boolean)] =
-    faces.flatMap((f) =>
-      f.boundaryIndex(edge).map{case (n,flipped) => (f, n, flipped)}
+    faces.flatMap(
+      (f) => f.boundaryIndex(edge).map { case (n, flipped) => (f, n, flipped) }
     )
 
   def normalArcs: Set[NormalArc] =
@@ -193,48 +250,54 @@ trait PureTwoComplex extends TwoComplex {
 }
 
 /**
- * A formal sum of elements of `A` with coefficients integers.
- */ 
-case class FormalSum[A](coeffs: Map[A, Int]){
+  * A formal sum of elements of `A` with coefficients integers.
+  */
+case class FormalSum[A](coeffs: Map[A, Int]) {
   val coeffVec = coeffs.toVector
 
   def ++(that: FormalSum[A]) = FormalSum.reduced(coeffVec ++ that.coeffVec)
 
-  def +(el: A) = FormalSum.reduced(coeffVec :+ (el -> 1) )
+  def +(el: A) = FormalSum.reduced(coeffVec :+ (el -> 1))
 
-  def -(el: A) = FormalSum.reduced(coeffVec :+ (el -> -1) )
+  def -(el: A) = FormalSum.reduced(coeffVec :+ (el -> -1))
 
-  def map[B](f: A => B) : FormalSum[B] = FormalSum.reduced(coeffVec.map{case (a, n) => (f(a), n)}) 
+  def map[B](f: A => B): FormalSum[B] =
+    FormalSum.reduced(coeffVec.map { case (a, n) => (f(a), n) })
 
-  def flatMap[B](f: A => FormalSum[B]) : FormalSum[B] = 
-    {
-      val cv = 
-        for {
-          (a, n) <- coeffVec
-          (b, m) <- f(a).coeffVec
-        } yield (b, n * m)
-      FormalSum.reduced(cv)
-    }
+  def flatMap[B](f: A => FormalSum[B]): FormalSum[B] = {
+    val cv =
+      for {
+        (a, n) <- coeffVec
+        (b, m) <- f(a).coeffVec
+      } yield (b, n * m)
+    FormalSum.reduced(cv)
+  }
 }
 
-object FormalSum{
+object FormalSum {
+
   /**
-   * reduce a formal sum by combining terms and removing zero terms
-   */ 
+    * reduce a formal sum by combining terms and removing zero terms
+    */
   def reduced[A](v: Vector[(A, Int)]) = {
-    val m = v.groupBy(_._1).view.mapValues{vc => vc.map(_._2).sum}.filter(_._2 != 0).toMap
+    val m = v
+      .groupBy(_._1)
+      .view
+      .mapValues { vc =>
+        vc.map(_._2).sum
+      }
+      .filter(_._2 != 0)
+      .toMap
     FormalSum(m)
   }
 
   /**
-   * second boundary map for homology
-   */ 
+    * second boundary map for homology
+    */
   def del2(c2: FormalSum[Polygon]) = c2.flatMap(_.del)
 
   /**
-   * first boundary map for homology
-   */
+    * first boundary map for homology
+    */
   def del1(c1: FormalSum[Edge]) = c1.flatMap(_.del)
 }
-
-
