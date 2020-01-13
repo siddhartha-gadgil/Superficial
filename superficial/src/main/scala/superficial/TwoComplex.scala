c3516ca6f449b976a7e486e95b4d794526bb1603
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-01-13 15:56:08 +0530

    refactored, some docs the file `TwoComplex.scala` now only contains the core space definitions.

--- superficial/src/main/scala/superficial/TwoComplex.scala
+++ superficial/src/main/scala/superficial/TwoComplex.scala
@@ -2,7 +2,7 @@ package superficial
 
 import Polygon.Index
 /**
-  * Abstract polygon, with given edges and vertices
+  * Abstract polygon, with given edges and vertices, i.e. a two-complex with a single face.
   * @param sides number of sides
   */
 trait Polygon extends TwoComplex {
@@ -14,16 +14,25 @@ trait Polygon extends TwoComplex {
 
   val boundary: Vector[Edge]
 
-  def del = {
+  /**
+   * the boundary as a formal sum
+   */
+  def del: FormalSum[Edge] = {
     val ob = boundary.collect{case oe: OrientedEdge => oe}
     assert(ob.size == boundary.size, "computing boundary where some edge is not oriented")
     val coeffVec = ob.map{case e => if (e.positivelyOriented) (e, 1) else (e.flip, -1)}
     FormalSum.reduced(coeffVec)
   }
 
+  /**
+   * the set of edges
+   */
   lazy val edges: Set[Edge] =
     boundary.toSet.flatMap((s: Edge) => Set(s, s.flip))
 
+  /**
+   * the set of vertices 
+   */
   val vertices: Set[Vertex]
 
   def boundaryIndex(e: Edge): Set[(Int, Boolean)] =
@@ -78,12 +87,15 @@ object Polygon {
 /**
   * A vertex in a two-complex
   */
-trait Vertex
+class Vertex
 
 /**
   * An oriented edge in a two-complex
   */
 trait Edge {
+  /**
+   * the same edge with the opposite orientation.
+   */
   def flip: Edge
 
   def terminal: Vertex
@@ -97,6 +109,9 @@ trait OrientedEdge extends Edge{
   val positivelyOriented : Boolean
 }
 
+/**
+ * An edge obtained by identifications
+ */ 
 case class QuotientEdge(edges: Set[Edge]) extends Edge {
   def flip = QuotientEdge(edges map (_.flip))
 
@@ -106,10 +121,13 @@ case class QuotientEdge(edges: Set[Edge]) extends Edge {
 
 }
 
+/**
+ * A vertex obtained by identifications
+ */ 
 case class QuotientVertex(vertices: Set[Vertex]) extends Vertex
 
 /**
-  *  A two complex
+  *  A polyheadral two complex, with faces polygons, a collection of edges and 
   */
 trait TwoComplex {
   def faces: Set[Polygon]
@@ -143,7 +161,7 @@ trait TwoComplex {
 }
 
 /**
-  * A two-complex with vertices and edges contained in faces.
+  * A two-complex with all vertices and edges contained in faces, hence determined by its faces.
   */
 trait PureTwoComplex extends TwoComplex {
   val faces: Set[Polygon]
@@ -155,6 +173,9 @@ trait PureTwoComplex extends TwoComplex {
     faces.map(_.vertices).foldLeft(Set.empty[Vertex])(_ union _)
 }
 
+/**
+ * A formal sum of elements of `A` with coefficients integers.
+ */ 
 case class FormalSum[A](coeffs: Map[A, Int]){
   val coeffVec = coeffs.toVector
 
@@ -178,138 +199,23 @@ case class FormalSum[A](coeffs: Map[A, Int]){
 }
 
 object FormalSum{
+  /**
+   * reduce a formal sum by combining terms and removing zero terms
+   */ 
   def reduced[A](v: Vector[(A, Int)]) = {
     val m = v.groupBy(_._1).view.mapValues{vc => vc.map(_._2).sum}.filter(_._2 != 0).toMap
     FormalSum(m)
   }
 
+  /**
+   * second boundary map for homology
+   */ 
   def del2(c2: FormalSum[Polygon]) = c2.flatMap(_.del)
 
+  /**
+   * first boundary map for homology
+   */
   def del1(c1: FormalSum[Edge]) = c1.flatMap(_.del)
 }
 
-/**
-  * A normal arc in a face
-  * @param initial the edge containing the initial point
-  * @param terminal the edge containing the final point
-  * @param face the face containing the arc
-  */
-case class NormalArc(initial: Index, terminal: Index, face: Polygon) {
-  val terminalEdge = face.boundary(terminal)
 
-  val initialEdge = face.boundary(initial)
-
-  def vertexLinking = math.abs(terminal - initial) == 1
-
-  def crosses(that: NormalArc) = (that.initial - initial) * (that.terminal - terminal) * (that.initial - terminal) * (that.terminal - initial) < 0
-
-}
-
-object NormalArc {
-  def enumerate(complex: TwoComplex): Set[NormalArc] =
-    for {
-      face <- complex.faces
-      initial <- face.indices
-      terminal <- face.indices
-      if terminal != initial
-    } yield NormalArc(initial, terminal, face)
-}
-
-case class NormalPath(edges: Vector[NormalArc]) {
-  edges.zip(edges.tail).foreach {
-    case (e1, e2) =>
-      require(e1.terminalEdge == e2.initialEdge || e1.terminalEdge == e2.initialEdge.flip,
-              s"terminal point on ${e1.terminalEdge} of $e1 is not initial point of $e2 on ${e2.initialEdge}")
-  }
-
-  def +:(arc: NormalArc) = NormalPath(arc +: edges)
-
-  def :+(arc: NormalArc) = NormalPath(edges :+ arc)
-//
-//  def appendOpt(arc: NormalArc): Option[NormalPath] =
-//    if (arc.initial == terminalEdge && arc != edges.last.flip) Some(this :+ arc)
-//    else None
-
-  val isClosed: Boolean = edges.last.terminalEdge == edges.head.initialEdge
-
-//  val initEdge: Edge = edges.head.initial
-//
-  val (terminalFace, terminalIndex) = edges.last.face -> edges.last.terminal
-
-  val terminalEdge =
-     terminalFace.boundary(terminalIndex)
-
-  val (initialFace, initialIndex) = edges.head.face -> edges.head.initial
-
-  val initialEdge =
-    initialFace.boundary(initialIndex)
-
-  def distinctFaces: Boolean = edges.map(_.face).distinct.size == edges.size
-
-  def linkingPair = edges.zip(edges.tail :+ edges.head).exists{case (x, y) => x.vertexLinking && y.vertexLinking}
-
-  def distinctEdges = {
-    val indEdg = edges.zipWithIndex
-    for {
-      (x, i) <- indEdg
-      (y, j) <- indEdg
-    } yield (x, y)
-  }
-
-  def withCross = distinctEdges.exists{case (x, y) => x.crosses(y)}
-
-  def geodesicCandidate = !(linkingPair || withCross)
-}
-
-object NormalPath {
-
-  @annotation.tailrec
-  def enumerateRec(complex: TwoComplex,
-                   maxAppendLength: Option[Int],
-                   p: NormalPath => Boolean,
-                   latest: Set[NormalPath],
-                   accum: Set[NormalPath]): Set[NormalPath] = {
-    if (maxAppendLength.contains(0) || latest.isEmpty) accum
-    else {
-      val newPaths =
-        (
-          for {
-            path <- latest
-            (face, i1) <- complex.edgeIndices(path.terminalEdge).map{case (f, i, _) => (f, i)} -
-              (path.terminalFace -> path.terminalIndex) -
-              (path.edges.last.face -> path.edges.last.initial)
-            i2 <- face.indices
-            if i2 != i1
-            arc = NormalArc(i1, i2, face)
-          } yield path :+ arc
-        ).filter(p)
-      enumerateRec(complex,
-                   maxAppendLength.map(_ - 1),
-                   p,
-                   newPaths,
-                   accum union newPaths)
-    }
-  }
-
-  /**
-    * recursively enumerate normal paths satisfying a hereditary condition with
-    * optional bound on length;
-    * terminates if the length is reached or no new paths were generated in the last step.
-    * @param complex  the two-complex
-    * @param maxLength an optional maximum length
-    * @param p a hereditary condition
-    * @return set of patbs with bounded length satisfying the condition
-    */
-  def enumerate(complex: TwoComplex,
-                maxLength: Option[Int] = None,
-                p: NormalPath => Boolean = (_) => true): Set[NormalPath] =
-    if (maxLength.exists(_ < 1)) Set()
-    else {
-      val lengthOne =
-        NormalArc
-          .enumerate(complex)
-          .map((arc) => NormalPath(Vector(arc)))
-          .filter(p)
-      enumerateRec(complex, maxLength.map(_ - 1), p, lengthOne, lengthOne)
-    }
-}
