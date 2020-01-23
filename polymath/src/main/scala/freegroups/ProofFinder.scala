d38f31797c921fa4d47e7b709321395e2a01caee
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-01-23 13:29:53 +0530

    plotting svg

--- polymath/src/main/scala/freegroups/ProofFinder.scala
+++ polymath/src/main/scala/freegroups/ProofFinder.scala
@@ -6,7 +6,7 @@ import monix.eval._, monix.tail._, cats.implicits._
 
 import monix.execution.Scheduler.Implicits.global
 
-import scala.concurrent._
+import scala.concurrent._, scala.util._
 
 import LinearNorm._, LinearNormProofs._
 import LinNormBound._
@@ -178,17 +178,39 @@ object ProofFinder {
   def getProof(w: Word): LinNormBound =
     LinearNormProofs.normProofTask(w, true).runSyncUnsafe()
 
-  def matchingPairs(pf: LinNormBound): Set[(Int, Int)] = 
+  def matchingPairs(pf: LinNormBound): Vector[(Int, Int)] = 
     pf match {
-      case Gen(n) => Set()
+      case Gen(n) => Vector()
       case ConjGen(n, pf) => 
         val inner = matchingPairs(pf)
-        inner.map{case (a, b) => (a + 1, b + 1)} + (0 -> (pf.word.ls.size + 1))
+        (0 -> (pf.word.ls.size + 1)) +: inner.map{case (a, b) => (a + 1, b + 1)} 
       case Triang(pf1, pf2) => 
         val first = matchingPairs(pf1)
         val shift = pf1.word.ls.size
-        first union (matchingPairs(pf2).map {case (a, b) => (a + shift, b+ shift)})
-      case PowerBound(baseword, n, pf) => Set()
-      case Empty => Set()
+        first ++ (matchingPairs(pf2).map {case (a, b) => (a + shift, b+ shift)})
+      case PowerBound(baseword, n, pf) => Vector()
+      case Empty => Vector()
     }
+
+  val rnd = new Random()
+
+  def randomWord(length: Int) : Word = 
+    Word(
+      (1 to length).toVector.map(_ => rnd.nextInt(4)).map{j => if (j > 1) j -1 else j - 2}
+      )    
+
+  def pairPath(a: Int, b: Int, index: Int) = {
+    val r = (b - a) * 6
+    val x = (a * 12)
+    val y = 750
+    val p = s"M $x $y A $r $r 0 0 1 ${x + (2 *r)} $y"
+    val colour = Map(1 -> "black", 2 -> "blue", -1 -> "red", -2 -> "green")(index)
+    s"""<path d="$p" stroke="$colour" fill="$colour" stroke-width="1" fill-opacity="0"/>"""
+  }
+
+  def pairsArcs(pairs: Vector[(Int, Int)], word: Word)
+    = pairs.map{case (a, b) => pairPath(a, b, word.ls(a))}.mkString(
+      """<svg xmlns="http://www.w3.org/2000/svg" width="1200" height="900">"""+ "\n",
+      "\n",
+      "</svg>")
 }
