e6f5e6e7c647290d9019bbe8ff5c5b92e0047cd7
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-01-24 15:54:38 +0530

    hugo cleaner

--- bindeploy.sh
+++ bindeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-rsync -av --progress math.iisc.ac.in:~/public_html/catg2020/bin/ CATG2020/static/bin/
-rsync -av CATG2020/static/bin/ CATG2020/notebooks/bin/
+rsync -av --progress math.iisc.ac.in:~/public_html/catg2020/bin/ CATG2020/notebooks/bin/ || true
 rsync -av CATG2020/notebooks/bin/superficial-*.jar CATG2020/static/bin/ || true
 ./mill superficial.docs
 cd CATG2020
