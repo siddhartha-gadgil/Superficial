ea751c53c73373bb4d18a5cd799ec1811fecceeb
root <root@LAPTOP-SQS99E6M.localdomain>
2020-01-30 23:19:14 +0530

    Edgepath methods

--- superficial/src/main/scala/superficial/EdgePath.scala
+++ superficial/src/main/scala/superficial/EdgePath.scala
@@ -1,8 +1,17 @@
 package superficial
 
-sealed trait EdgePath{
+sealed trait EdgePath{ edgePath =>
+    import EdgePath._
     val initial: Vertex
     val terminal: Vertex
+
+    def ++(path:EdgePath): EdgePath = {
+        path match{
+            case Constant(initial) => edgePath
+            case Append(Constant(init), e) => Append(edgePath, e)
+            case Append(Append(ep, e1), e2) => Append((edgePath ++ Append(ep,e1)), e2)
+        }
+    }
 }
 
 object EdgePath{
@@ -17,15 +26,30 @@ object EdgePath{
         s"the terminal vertex ${init.terminal} of the initial segment $init is not the initial vertex ${last.initial} of the final edge $last.")
     }
 
-    def length(path: EdgePath) : Int =
+    def length(path: EdgePath) : Int = {
         path match {
             case Append(init, last) => length(init) + 1
             case Constant(initial) => 0
         }
+    }
+     
+    def apply(v: Vector[Edge]) : EdgePath = {
+        assert(v.nonEmpty)
+        v match {
+            case y+:(z+:ys) => Append(apply(v.init), v.last)
+            case Vector(y) => Append(Constant(y.initial), y)
+        }
+    }
 
-    def apply(v: Vector[Edge]) : EdgePath = ??? // implement this
+    def boundary(poly: Polygon) : EdgePath = {
+        EdgePath(poly.boundary)
+    }
 
-    def boundary(poly: Polygon) : EdgePath = ??? // implement this
-
-    def isReduced(path: EdgePath) : Boolean = ??? // implement
+    def isReduced(path: EdgePath) : Boolean = {
+        path match {
+            case Append(Append(ep, e1),e2) => isReduced(Append(ep,e1)) && (e1 != e2.flip)
+            case Append(Constant(initial), e) => true
+            case Constant(initial) => true
+        }
+    }
 }
