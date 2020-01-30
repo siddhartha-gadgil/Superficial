9e860483583066f2974b22ba305a76172fc12147
root <root@LAPTOP-SQS99E6M.localdomain>
2020-01-30 23:26:17 +0530

    Commenting

--- superficial/src/main/scala/superficial/EdgePath.scala
+++ superficial/src/main/scala/superficial/EdgePath.scala
@@ -5,6 +5,7 @@ sealed trait EdgePath{ edgePath =>
     val initial: Vertex
     val terminal: Vertex
 
+    //Concatenation for EdgePaths
     def ++(path:EdgePath): EdgePath = {
         path match{
             case Constant(initial) => edgePath
@@ -15,10 +16,13 @@ sealed trait EdgePath{ edgePath =>
 }
 
 object EdgePath{
+
+    //Constructor for a constant EdgePath
     final case class Constant(initial: Vertex) extends EdgePath{
         val terminal: Vertex = initial
     }
 
+    //Inductive constructor for EdgePaths
     final case class Append(init: EdgePath, last: Edge) extends EdgePath{
         val initial: Vertex = init.initial
         val terminal: Vertex = last.terminal
@@ -26,6 +30,7 @@ object EdgePath{
         s"the terminal vertex ${init.terminal} of the initial segment $init is not the initial vertex ${last.initial} of the final edge $last.")
     }
 
+    //Finds the length of an EdgePath
     def length(path: EdgePath) : Int = {
         path match {
             case Append(init, last) => length(init) + 1
@@ -33,6 +38,7 @@ object EdgePath{
         }
     }
      
+    //Constructing EdgePaths from vectors of edges
     def apply(v: Vector[Edge]) : EdgePath = {
         assert(v.nonEmpty)
         v match {
@@ -41,10 +47,12 @@ object EdgePath{
         }
     }
 
+    //Converts the boundary of a polygon to an EdgePath
     def boundary(poly: Polygon) : EdgePath = {
         EdgePath(poly.boundary)
     }
 
+    //Checks if an EdgePath is reduced
     def isReduced(path: EdgePath) : Boolean = {
         path match {
             case Append(Append(ep, e1),e2) => isReduced(Append(ep,e1)) && (e1 != e2.flip)
