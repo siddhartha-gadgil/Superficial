fe385062e84fb2a6dff08eb79bb15e9323de3120
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-02-04 10:00:01 +0530

    standard surfaces

---
+++ superficial/src/main/scala/superficial/StandardSurface.scala
@@ -0,0 +1,33 @@
+package superficial
+
+class StandardSurface(genus: Int) extends TwoComplex{surface =>
+    def faces: Set[Polygon] = Set(face)
+    def edges: Set[Edge] = face.edges
+    def vertices: Set[Vertex] = Set(vertex)
+
+    lazy val face : Polygon = Polygon(boundary)
+
+    import StandardSurface._
+
+    lazy val boundary : Vector[Edge] = (0 until genus).flatMap(
+        k => Vector(A(k, surface, true), B(k, surface, true), A(k, surface, false), B(k, surface, false))
+    ).toVector
+
+    val vertex = new Vertex
+
+    
+}
+
+object StandardSurface{
+    case class A(index: Int, surf: StandardSurface, positive: Boolean) extends Edge{
+        def flip: Edge = A(index, surf, !positive)
+        lazy val terminal: Vertex = surf.vertex
+        lazy val initial: Vertex = surf.vertex
+    }
+    case class B(index: Int, surf: StandardSurface, positive: Boolean) extends Edge{
+        def flip: Edge = B(index, surf, !positive)
+        lazy val terminal: Vertex = surf.vertex
+        lazy val initial: Vertex = surf.vertex
+    }
+   
+}
