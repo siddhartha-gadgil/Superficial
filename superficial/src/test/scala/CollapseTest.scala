81b189582b070383f2d5b61ef8f243ace20f5ce4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-02-04 10:43:59 +0530

    example moved from test to body

--- superficial/src/test/scala/CollapseTest.scala
+++ superficial/src/test/scala/CollapseTest.scala
@@ -2,23 +2,6 @@ package superficial
 
 import utest._
 
-object SphereComplex {
-  case object X extends Vertex
-  case object Y extends Vertex
-  case object Z extends Vertex
-
-  case object A extends EdgePair(X, Y)
-  case object B extends EdgePair(Y, Z)
-  case object C extends EdgePair(Z, X)
-
-  val upper = Polygon(Vector(A.Positive, B.Positive, C.Positive))
-  val lower = Polygon(Vector(C.Negative, B.Negative, A.Negative))
-
-  val doubleTriangle: TwoComplex = TwoComplex.pure(upper, lower)
-
-  val doubleBigon: TwoComplex = doubleTriangle.collapseEdge(A.Positive)
-  val doubleMonogon = TwoComplex.allCollapsed(doubleTriangle)
-}
 import SphereComplex._
 
 object CollapseTest extends TestSuite {
@@ -43,7 +26,7 @@ object CollapseTest extends TestSuite {
         }
       }
     }
-    "AllComplexes" - {
+    "AllSphereComplexes" - {
       "areConnected" - {
         assert(
           doubleTriangle.isConnectedComplex && doubleBigon.isConnectedComplex && doubleMonogon.isConnectedComplex
