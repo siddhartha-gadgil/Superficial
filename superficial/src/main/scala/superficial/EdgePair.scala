86afd73b37752d0875f340464425679ed9289957
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-02-24 15:33:14 +0530

    more standardization

--- superficial/src/main/scala/superficial/EdgePair.scala
+++ superficial/src/main/scala/superficial/EdgePair.scala
@@ -7,9 +7,9 @@ class EdgePair(val initial: Vertex, val terminal: Vertex) { pair =>
 }
 
 object EdgePair{
-  case class Oriented(pair: EdgePair, positive: Boolean) extends Edge{
-    lazy val flip: Edge = Oriented(pair, !positive)
-    lazy val terminal: Vertex = if (positive) pair.terminal else pair.initial
-    lazy val initial: Vertex = if (positive) pair.initial else pair.terminal
+  case class Oriented(pair: EdgePair, positivelyOriented: Boolean) extends OrientedEdge{
+    lazy val flip: OrientedEdge = Oriented(pair, !positivelyOriented)
+    lazy val terminal: Vertex = if (positivelyOriented) pair.terminal else pair.initial
+    lazy val initial: Vertex = if (positivelyOriented) pair.initial else pair.terminal
   }
 }
--- superficial/src/main/scala/superficial/StandardSurface.scala
+++ superficial/src/main/scala/superficial/StandardSurface.scala
@@ -42,15 +42,15 @@ class StandardSurface(val genus: Int) extends TwoComplex{surface =>
 }
 
 object StandardSurface{
-    case class A(index: Int, surf: StandardSurface, positive: Boolean) extends Edge{
+    case class A(index: Int, surf: StandardSurface, positivelyOriented: Boolean) extends OrientedEdge{
         assert(0 < index && index <= surf.genus, s"index $index invalid for genus ${surf.genus} surface")
-        def flip: Edge = A(index, surf, !positive)
+        lazy val flip: OrientedEdge = A(index, surf, !positivelyOriented)
         lazy val terminal: Vertex = surf.vertex
         lazy val initial: Vertex = surf.vertex
     }
-    case class B(index: Int, surf: StandardSurface, positive: Boolean) extends Edge{
+    case class B(index: Int, surf: StandardSurface, positivelyOriented: Boolean) extends OrientedEdge{
         assert(0 < index && index <= surf.genus, s"index $index invalid for genus ${surf.genus} surface")
-        def flip: Edge = B(index, surf, !positive)
+        lazy val flip: OrientedEdge = B(index, surf, !positivelyOriented)
         lazy val terminal: Vertex = surf.vertex
         lazy val initial: Vertex = surf.vertex
     }
